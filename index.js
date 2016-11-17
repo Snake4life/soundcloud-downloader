256be3e53150d43c576c3f332000acd6c7983f44
Shortt <alexander.shortt18@bcp.org>
2016-11-17 14:34:20 -0800

    verify if this works

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
+    console.log("Welcome to homepage");
     response.render('pages/index');
 });
+
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
