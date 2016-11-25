8d593faaad9bad3eaf2f3c85b1f65037f00fe8bc
Shortt <alexander.shortt18@bcp.org>
2016-11-24 16:44:25 -0800

    modular

--- index.js
+++ index.js
@@ -21,38 +21,70 @@ app.get('/', function (request, response) {
 });
 
 app.get('/getSound', function (req, res) {
+    var query = require('url').parse(req.url, true).query;
+    var link = query.link;
+    var artist = query.artist;
+    var title = query.title;
+    var genre = query.genre;
+    var album = query.album;
+    var album_art = query.album_art;
+    
     var exePath = path.resolve(__dirname, './youtube-dl');
     console.log("path: " + exePath);
     fs.chmodSync('youtube-dl', 0777);
-    exec(exePath + " https://soundcloud.com/sanholobeats/light", function (error, stdout, stderr) {
+    exec(exePath + " " + link, function (error, stdout, stderr) {
         if (error) {
             console.log(error);
             throw error;
         }
         console.log(stdout);
-        var dest = stdout.split("Destination: ")[1];
-        console.log("dest: " + dest + "<<");
-        var other = dest.replace(dest.split(".mp3")[1], "");
-        console.log("other: " + other + "<<");
+        var dest = stdout.split("Destination: ")[1].replace(dest.split(".mp3")[1], "");
 
-        var songBuffer = fs.readFileSync(__dirname + "/" + other);
-        var coverBuffer = fs.readFileSync(__dirname + "/light.jpg");
+        var songBuffer = fs.readFileSync(__dirname + "/" + dest);
+        var coverBuffer = fs.readFileSync(album_art);
 
         var writer = new ID3Writer(songBuffer);
-        writer.setFrame('TIT2', 'Light')
-            .setFrame('TPE1', ['San Holo'])
-            .setFrame('TALB', 'Light')
-            .setFrame('TCON', ['Future Bass'])
+        writer.setFrame('TIT2', title)
+            .setFrame('TPE1', [artist])
+            .setFrame('TALB', album)
+            .setFrame('TCON', [genre])
             .setFrame('APIC', coverBuffer);
         writer.addTag();
 
         var taggedSongBuffer = new Buffer(writer.arrayBuffer);
-        fs.writeFileSync('San Holo - Light.mp3', taggedSongBuffer);
+        fs.writeFileSync(artist + " - " + title + '.mp3', taggedSongBuffer);
         
-        var file = __dirname + '/San Holo - Light.mp3';
+        var file = __dirname + artist + " - " + title + '.mp3';
         res.download(file);
     });
 });
+
+/*
+array of strings:
+
+TPE1 (song artists)
+TCOM (song composers)
+TCON (song genres)
+
+string
+
+TIT2 (song title)
+TALB (album title)
+TPE2 (album artist)
+TRCK (song number in album): '5' or '5/10'
+TPOS (album disc number): '1' or '1/3'
+USLT (unsychronised lyrics)
+TPUB (label name)
+
+integer
+
+TLEN (song duration in milliseconds)
+TYER (album release year)
+
+arrayBuffer
+
+APIC (song cover): works with jpeg, png, gif, webp, tiff, bmp and ico
+*/
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
