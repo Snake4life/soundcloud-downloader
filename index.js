b8b1e17e0e2856206a24202dfe04591b4319921e
Shortt <alexander.shortt18@bcp.org>
2016-12-19 16:28:37 -0800

    end res stream; trying to fix bug where it crashes every few times

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ app.get('/getSound', function (req, res) {
                     res.send("<script>\
                          window.close();\
                          </script>");
+                    res.end();
                 });
             }
         };
